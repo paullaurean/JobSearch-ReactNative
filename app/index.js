4ace4ef79d9211cf24c13d0330f57c41e3beee30
Paul Laurean <paullaurean.franco@ssu.edu.ph>
2023-07-06 13:07:33 +0800

    Added assets, components, styles folder and it's working

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 import React from 'react'
 
+import {  COLORS, icons, images, SIZES} from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
 const Home = () => {
+  
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <Stack.Screen
+      options={{
+        headerStyle: { backgroundColor: COLORS.lightWhite}
+      }}/>
+    </SafeAreaView>
   )
 }
 
