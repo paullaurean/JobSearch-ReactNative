520b5ece76f1746adb1942759a3998bbf04d65be
Paul Laurean <paullaurean.franco@ssu.edu.ph>
2023-07-11 15:09:55 +0800

    added image url checker feature

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,5 +1,6 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
+import {checkImageURL} from '../../../../utils'
 
 import styles from './popularjobcard.style';
 
@@ -11,7 +12,9 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress}) => {
       >
         <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
           <Image
-            source={{ uri: item.employer_logo}}
+            source={{ uri: checkImageURL(item.employer_logo)
+            ? item.employer_logo
+            : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'}}
             resizeMode="contain"
             style={styles.logoImage}
           />
